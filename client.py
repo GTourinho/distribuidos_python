a988afca9c208d50c9081bf6022c374b41110a3c
Paulo Bomfim <paulobommfim@gmail.com>
2021-12-03 16:57:49 -0300

    chore: add comments

--- client.py
+++ client.py
@@ -1,8 +1,17 @@
 from socket import *
 import sys
 
+# Definições da classe de cliente
+# Importamos bibliotecas de socket para podermos simular as operações de maneira 
+# mais real dentro da rede.
+# Estaremos definindo endereços padrões, para não causarmos conflitos etc.
+
 class Client:
-    
+    # Essa é a função de inicalização, onde definimos os valores padrões
+    # para o nosso client.
+    # Iniciamos sempre solicitando os dados do usuario, para gravarmos
+    # no nosso servidor.
+    # Utilizamos um padrão para identificalos, com nome e RG.
     def __init__(self):
         self.socket = socket(AF_INET, SOCK_STREAM)
         self.socket.connect(('127.0.0.1',9000))
@@ -12,10 +21,15 @@ class Client:
         self.socket.send(msg.encode())
         self.socket.close()
 
+    # Função connect responsável por iniciar a conexao ao endereço determinado.
     def connect(self):
         self.socket = socket(AF_INET, SOCK_STREAM)
         self.socket.connect(('127.0.0.1',9000))
         
+    # Função auxiliar que servirá ao longo do programa, enviando
+    # mensagens ao usuário, via console.
+    # Existem diversas opções, mas todas são realizadas enviando mensagens
+    # ao servidor e recebendo respostas, para designarmos a tela.
     def sendmsg(self):
         print('Digite o número de acordo com a opção que deseja:')
         print('1: Saque')
@@ -27,6 +41,7 @@ class Client:
 
         print('')
         
+        # Opção de saque, retorna mensagem de sucesso ou falha
         if msg == '1':
             print('Digite a quantia')
             msg = msg + self.id + '.' + input().strip()
@@ -38,6 +53,7 @@ class Client:
             else:
                 print('Quantia insuficiente!')
             
+        # Opção de depósito, retorna mensagem de sucesso
         elif msg == '2':
             print('Digite a quantia')
             msg = msg + self.id + '.' + input().strip()
@@ -45,6 +61,9 @@ class Client:
             self.socket.send(msg.encode())
             print('Sua solicitação foi realizada com sucesso!')
 
+        # Opção de transferência, retorna mensagem de sucesso ou falha, pois
+        # além de checar o saldo, verifica a existência do destinatário no
+        # sistema.
         elif msg == '3':
             print('Digite a conta do destinatário (id)')
             destinatario = input()
@@ -60,6 +79,7 @@ class Client:
             else:
                 print('Quantia insuficiente!')
 
+        # Opção de saldo, retorna o valor
         elif msg == '4':
             msg = msg + self.id
             self.connect()
@@ -67,12 +87,16 @@ class Client:
             saldo = self.socket.recv(10).decode()
             print('Seu saldo é:', saldo)
             
+        # Opção de saida, finaliza o servidor e sai do sistema
+        # atual, o client.
         elif msg == '5':
             self.connect()
             self.socket.send(msg.encode())
             self.socket.close()
             sys.exit()
 
+        # Tratamento de erro para caso o usuário selecioanr uma
+        # opção inválida
         else:
             self.connect()
             print('Operação inválida!')
--- server.py
+++ server.py
@@ -1,19 +1,38 @@
 from socket import *
 
+# Definições da classe do servidor.
+# Importamos bibliotecas de socket para podermos simular as operações de maneira 
+# mais real dentro da rede.
+# Estaremos definindo endereços padrões, para não causarmos conflitos etc.
+
 class Server:
+    # Essa é a função de inicalização, onde definimos os valores padrões
+    # para o nosso socket.
+    # Além disso, inicalizamos um array para armazenarmos os dados
+    # dos possíveis clientes.
     def __init__(self):
         self.socket = socket(AF_INET, SOCK_STREAM)
         self.socket.bind(('127.0.0.1',9000))
         self.clients = {}
         
+    # Operação de escuta, onde aguardamos e tratamos solicitações por
+    # parte do cliente, a partir dos pacotes.
     def listen(self):
         self.socket.listen(5)
         (client, dummy) = self.socket.accept()
         packet = client.recv(50).decode()
         
+        # Tratamento que fazemos para o primeiro momento de cadastro,
+        # onde recebemos a informação do cliente, guardando o seu id
+        # e inicalizando o seu saldo zerado.
+        # Em todas as opções seguintes, observamos a requisição a partir do
+        # caractere '.' ; o que acontece é que separamos valores e flags, 
+        # entre pontos " . " para sabermos o que é ID e o que é valor.
         if packet[:3] == 'id_':
             self.clients[packet[3:]] = 0
             print('id do cliente:', packet[3:])
+        
+        # Opção de saque, retorna mensagem de sucesso ou falha (0 ou 1)
         elif packet[0] == '1':
             id, quantia = packet[1:].split('.')
 
@@ -25,10 +44,16 @@ class Server:
                 client.sendall(msg.encode())
                 self.clients[id] -= abs(int(quantia))
 
+        # Opção de depósito, onde somente adicionamos quantia ao cliente,
+        # sem retorno.
         elif packet[0] == '2':
             id, quantia = packet[1:].split('.')
             self.clients[id] += abs(int(quantia))
-            
+
+        # Opção de transferência, retorna mensagem de sucesso ou falha, pois
+        # além de checar o saldo, verifica a existência do destinatário no
+        # sistema. Caso tudo esteja de acordo, realizamos as operações
+        # matemáticas.
         elif packet[0] == '3':
             id, id2, quantia = packet[1:].split('.')
             if abs(int(quantia)) > self.clients[id]:
@@ -43,7 +68,7 @@ class Server:
                 self.clients[id2] += abs(int(quantia))
                 client.sendall(msg.encode())
                 
-            
+        # Opção de saldo, retorna o valor do cliente específico.
         elif packet[0] == '4':
             client.sendall(str(self.clients[packet[1:]]).encode())
             
